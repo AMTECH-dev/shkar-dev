7a4bd31959181c725a11dcfcd336a11733203b38
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2021-04-27 18:10:47 +0300

    small refactoring

--- httpserver/VladislavBakshanskij/Server2/src/com/company/handlers/ImageHandler.java
+++ httpserver/VladislavBakshanskij/Server2/src/com/company/handlers/ImageHandler.java
@@ -6,7 +6,6 @@ import com.company.utils.FileUtils;
 import com.sun.net.httpserver.HttpExchange;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -16,23 +15,24 @@ import java.net.URISyntaxException;
 public class ImageHandler extends StaticHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        ImageIcon icon = new ImageIcon(Main.class.getResource("static/img/cat.png"));
-        BufferedImage bufferedImage = new BufferedImage(icon.getIconWidth(),
-                icon.getIconHeight(), BufferedImage.TYPE_INT_RGB);
+        BufferedImage catImage = readImage("cat.png");
 
-        Graphics graphics = bufferedImage.getGraphics();
-        graphics.drawImage(icon.getImage(), 0, 0, null);
-
-        graphics.drawImage(ImageIO.read(Main.class.getResource("static/img/triangle.png")), 0, icon.getIconHeight() / 2, null);
+        Graphics graphics = catImage.getGraphics();
+        graphics.drawImage(catImage, 0, 0, null);
+        graphics.drawImage(readImage("triangle.png"), 0, catImage.getHeight() / 2, null);
         graphics.dispose();
 
         try {
-                File outputFile = new File(Main.class.getResource("static/img/catWithTriangle.png").toURI());
-            ImageIO.write(bufferedImage, "png", outputFile);
+            File outputFile = new File(Main.class.getResource("static/img/catWithTriangle.png").toURI());
+            ImageIO.write(catImage, "png", outputFile);
 
             byte[] response = FileUtils.readAllBytesFromStaticFile("img/catWithTriangle.png");
             sendResponse(httpExchange, HttpCode.SUCCESS, response);
         } catch (URISyntaxException ignored) {
         }
+    }
+
+    private BufferedImage readImage(String imageName) throws IOException {
+        return ImageIO.read(Main.class.getResource("static/img/" + imageName));
     }
 }
