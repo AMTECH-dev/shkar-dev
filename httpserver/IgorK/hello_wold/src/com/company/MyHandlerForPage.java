0e808f1c38ac899e7db2bfe6340373fdca578bd7
Igor Kocherzhenko <k.iv86@yandex.ru>
2021-04-27 10:57:45 +0300

    add '/page.html'

---
+++ httpserver/IgorK/hello_wold/src/com/company/MyHandlerForPage.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+class MyHandlerForPage implements HttpHandler {
+    String form;
+    StringBuilder sb = new StringBuilder();
+    public void handle(HttpExchange t) throws IOException {
+        returnPage();
+
+        byte[] response = form.getBytes(StandardCharsets.UTF_8);
+        t.getResponseHeaders().add("Content-Type", "text/html; charset=UTF-8");
+        t.sendResponseHeaders(200, response.length);
+        OutputStream os = t.getResponseBody();
+        os.write(response);
+        os.close();
+    }
+    public void returnPage() {
+
+        BufferedReader io = null;
+        try {
+            io = new BufferedReader(new FileReader("page.html"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            try {
+                if (!((form = io.readLine()) != null)) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            sb.append(form);
+        }
+        form = sb.toString();
+        try {
+            io.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
