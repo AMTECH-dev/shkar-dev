bd12811f934b4342ac6275953da8ebdf1c9aa90d
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2021-04-28 12:13:23 +0300

    ...

--- httpserver/VladislavBakshanskij/Server2/src/com/company/factories/LoggerFactory.java
+++ httpserver/VladislavBakshanskij/Server2/src/com/company/factories/LoggerFactory.java
@@ -7,13 +7,15 @@ public class LoggerFactory {
     private static final String PATH_TO_LOG_DIR = "log/";
     private static final String LOG_FILE_EXTENSION = "log";
 
+    private static final int FILE_SIZE_IN_BYTE = 1000_000_000;
+    private static final boolean APPEND = true;
+    private static final int FILE_COUNT = 3;
+
     public static Logger createLoggerWithConfiguration(Class<?> clazz) {
         Logger logger = Logger.getLogger(clazz.getName());
         String filePattern = PATH_TO_LOG_DIR + clazz.getSimpleName() + "%u." + LOG_FILE_EXTENSION;
         try {
-            boolean append = true;
-            int fileSizeByte = 1000_000_000;
-            logger.addHandler(new FileHandler(filePattern, fileSizeByte, 3, append));
+            logger.addHandler(new FileHandler(filePattern, FILE_SIZE_IN_BYTE, FILE_COUNT, APPEND));
         } catch (Exception ignored) {
         }
 
