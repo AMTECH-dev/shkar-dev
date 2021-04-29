647d3bd987f01873985ac3ba513ff2cf90caa4af
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2021-04-29 22:56:08 +0300

    added create folder if not exist.

--- httpserver/VladislavBakshanskij/simplehttpserver/src/com/company/factories/LoggerFactory.java
+++ httpserver/VladislavBakshanskij/simplehttpserver/src/com/company/factories/LoggerFactory.java
@@ -2,20 +2,20 @@ package com.company.factories;
 
 import com.company.utils.FileUtils;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class LoggerFactory {
-    private static final String DEFAULT_CONFIG_FILE_NAME = "application.properties";
+    private static final String CONFIG_FILE_NAME = "application.properties";
+    private static final String LOG_FOLDER = "./log/";
 
     public static Logger createLogger(Class<?> clazz) {
-        return createLoggerFromConfiguration(clazz, DEFAULT_CONFIG_FILE_NAME);
-    }
-
-    public static Logger createLoggerFromConfiguration(Class<?> clazz, String configFileName) {
         Logger logger = Logger.getLogger(clazz.getName());
         try {
-            LogManager.getLogManager().readConfiguration(FileUtils.getResourceAsStream(configFileName));
+            if (Files.notExists(Paths.get(LOG_FOLDER))) Files.createDirectory(Paths.get(LOG_FOLDER));
+            LogManager.getLogManager().readConfiguration(FileUtils.getResourceAsStream(CONFIG_FILE_NAME));
         } catch (Exception ignored) {
         }
 
