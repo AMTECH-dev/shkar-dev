0a60f5e1da28c9bf0cd589deb874c07d7b9c1dea
KiraLis39 <alexnestor2020@gmail.com>
2021-04-29 12:24:04 +0300

    refactoring all

---
+++ httpserver/kiralis39/amtech/src/main/java/amtech/registry/Configurations.java
@@ -0,0 +1,7 @@
+package amtech.registry;
+
+import java.util.Properties;
+
+public class Configurations {
+    public static Properties globalConfig = new Properties();
+}
---
+++ httpserver/kiralis39/amtech/src/main/java/amtech/registry/ReturnCodes.java
@@ -0,0 +1,8 @@
+package amtech.registry;
+
+public class ReturnCodes {
+    public static final int OK = 200;
+    public static final int NO_CONTENT = 204;
+    public static final int BAD_REQUEST = 400;
+    public static final int ERR_404 = 404;
+}
