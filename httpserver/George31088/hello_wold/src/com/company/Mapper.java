c454c0f12844284adcb2dda86295a8a37cbd61bf
George31088 <tchuikog@gmail.com>
2021-04-29 12:09:53 +0300

    Json done

---
+++ httpserver/George31088/hello_wold/src/com/company/Mapper.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapper {
+    public static Map<String, String> getParametersFromQueryString(String query) {
+        String[] pairs = query.split("&"); // a=1&b=2 -> [a=1, b=2]
+        Map<String, String> map = new HashMap<>();
+        for (String s : pairs) {
+            String[] keyValue = s.split("="); // a= ->[a]
+            if (keyValue.length > 1) {
+                map.put(keyValue[0], keyValue[1]);}
+        }
+        return map;
+    }
+
+    public static String toJSON(Object o) {
+        String gson = new Gson().toJson(o);
+        return gson;
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> parametersFromQueryString = getParametersFromQueryString("a=1&b=2");
+        System.out.println(parametersFromQueryString);
+    }
+}
